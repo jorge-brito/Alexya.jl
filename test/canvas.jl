10832af744d866d75b22691b8f00291bce921e6e
Jorge Brito <jorge.brito.json@gmail.com>
2021-02-21 19:09:19 -0300

    A few changes

--- test/canvas.jl
+++ test/canvas.jl
@@ -1,117 +1,72 @@
-@testset "Canvas" begin
+@testset "Canvas Test" begin
     canvas = Canvas()
     win = Window(canvas, 800, 600; title = "Canvas test")
-    app = DrawingApp(win, canvas; framerate = 60)
+    lf = Ref{DateTime}(now())
 
     @test canvas isa Alexya.GtkCanvas
 
-    function setup(w, h)
-        @test w == 800
-        @test h == 600
-        println("Starting...")
-    end
-
     function draw(w, h)
+        fps = getFPS!(lf) |> round
         background("black")
         sethue("white")
         x, y = rand(0:w), rand(0:h)
         circle(x, y, 10, :fill)
+        fontsize(24)
+        text("Current FPS is $fps frames/s", w/2, 40, halign=:center)
     end
 
-    loop!(app, setup, draw)
+    loop!(draw, win, canvas)
 end
 
-@testset "Global canvas" begin
-    createCanvas(800, 600; title = "Canvas Test")
+@testset "Create canvas" begin
+    createCanvas(800, 600; title = "Create canvas")
+    lf = Ref{DateTime}(now())
 
-    @add slider = Slider(1:100; vexpand = false, @hexpand, @margin(5, 10))
-    @add Label("Hello")
-    @add Button("Ok")
+    radius = @create Slider(1:200; startat = 20)
 
     function setup(w, h)
-        @test slider isa Alexya.GtkScale
-        @test w == 800
-        @test h == 600
-        println("Starting...")
+        println("Creating canvas with width of $w and height of $h")
     end
 
     function draw(w, h)
+        fps = getFPS!(lf) |> round
         background("black")
         sethue("white")
         x, y = rand(0:w), rand(0:h)
-        circle(x, y, value(slider), :fill)
+        circle(x, y, 10, :fill)
+        fontsize(24)
+        text("Current FPS is $fps frames/s", w/2, 40, halign=:center)
     end
 
     loop!(setup, draw)
 end
 
-@testset "Canvas layout" begin
-    uselayout(HPanels{120})
-    createCanvas(800, 600; title = "Canvas Test")
+mousepos = Point(0, 0)
 
-    @add slider = Slider(1:100; @hexpand)
-    @add Label("Hello")
-    @add Button("Ok")
+@testset "Canvas with controls & events" begin
+    @layout aside()
+    createCanvas(800, 600; title = "Canvas with controls & events")
+
+    lf = Ref{DateTime}(now())
+    radius = @create Slider(1:200; startat = 20)
 
     function setup(w, h)
-        @test slider isa Alexya.GtkScale
-        @test w == 800
-        @test h == 600
-        println("Starting...")
+        println("Creating canvas with width of $w and height of $h")
+        global mousepos = Point(w/2, h/2)
     end
 
     function draw(w, h)
+        fps = getFPS!(lf) |> round
         background("black")
         sethue("white")
-        x, y = rand(0:w), rand(0:h)
-        circle(x, y, value(slider), :fill)
+        circle(mousepos, value(radius), :fill)
+        fontsize(24)
+        text("Current FPS is $fps frames/s", w/2, 40, halign=:center)
+    end
+
+    onmousemotion!() do w, event
+        global mousepos = Point(event.x, event.y)
     end
 
     loop!(setup, draw)
-end
-
-global mousepos = [0, 0]
-global lastmousepos = mousepos
-global radius = 5
-
-@testset "Events" begin
-    createCanvas(800, 600)
-
-    function update(args...)
-        background("black")
-        sethue("white")
-        circle(point(mousepos), radius, :fill)
-    end
-
-    onmousemotion!(0) do w, event
-        global mousepos = [event.x, event.y]
-    end
-
-    onmousemotion!(1) do w, event
-        global radius = abs([event.x, event.y] - lastmousepos)
-    end
-
-    for n in 1:3
-        onclick!(n) do w, event
-            println("Mouse $n pressed!")
-            if n == 1
-                global lastmousepos = [event.x, event.y]
-            end
-        end
-    
-        onclicked!(n) do w, event
-            println("Mouse $n released!")
-        end
-    end
-
-    onkeypress!() do w, event
-        println("Key $(event.keyval) pressed!")
-    end
-
-    onkeyrelease!() do w, event
-        @show event.keyval == key("w")
-        println("Key $(event.keyval) released!")
-    end
-
-    loop!(update)
 end
