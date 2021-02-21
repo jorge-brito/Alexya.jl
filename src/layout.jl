4d522454adfcfb31163c12ad56b08eb3321b61ae
Jorge Brito <jorge.brito.json@gmail.com>
2021-02-21 19:08:53 -0300

    Recreated layout system

--- src/layout.jl
+++ src/layout.jl
@@ -1,39 +1,79 @@
-abstract type Layout end
+function nolayout(window::GtkWindow, canvas::GtkCanvas)
+    add!(window, canvas)
+    return function(child::T) where T <: GtkWidget
+        @warn """
+        
+        Attempt to add widget of type $T but no layout is being used.
+        If you want to use other widgets with the canvas call the `uselayout!` 
+        function (or the @layout macro) and pass a layout. Example:
 
-function createLayout end
+                `@layout aside(type = :horizontal)`
+            
+        You can also implement your own layouts.
 
-mutable struct Panels{T, N} <: Layout
-    box::GtkContainer
-    panels::GtkPaned
-    empty::Bool
-end
-
-const VPanels{N} = Panels{:v, N}
-const HPanels{N} = Panels{:h, N}
-
-function createLayout(::Type{Panels{T, N}}, canvas::GtkCanvas, win::GtkWindow) where {T, N}
-    box = Box(T == :v ? :h : :v; @margin(10), spacing = 10)
-    position = T == :h ? width(win) : height(win)
-    panels = Paned([canvas, box], T; position)
-    add!(win, panels)
-    return Panels{T, N}(box, panels, true)
-end
-
-function add!(w::GtkWidget, layout::Panels{T, N}, win::GtkWindow) where {T, N}
-    if layout.empty
-        wd = width(win)
-        ht = height(win)
-        if T == :h
-            resize!(win, wd + N, ht)
-            position = wd
-        else
-            resize!(win, wd, ht + N)
-            position = ht
-        end
-        set!(layout.panels; position)
-        layout.empty = false
+        """
     end
+end
 
-    add!(layout.box, w)
-    return w;
+function aside(::Val{:vertical}, size::Int, reverse::Bool)
+    return function(window, canvas)
+        w = width(window)
+        box = Box(:v; @margin(20, 10))
+        _empty = true
+
+        pn = Paned(:h; position = w) do
+            reverse && return box, canvas
+            return canvas, box
+        end
+
+        add!(window, pn)
+        resizeWidth!(window, w + size)
+
+        return (child) -> add!(box, child)
+    end
+end
+
+aside(::Val{:v}, args...) = aside(Val(:vertical), args...)
+
+function aside(::Val{:horizontal}, size::Int, reverse::Bool)
+    return function(window, canvas)
+        h = width(window)
+        box = Box(:h; @margin(20, 10))
+
+        pn = Paned(:v; position = h) do
+            reverse && return box, canvas
+            return canvas, box
+        end
+
+        add!(window, pn)
+        resizeHeight!(window, h + size)
+        
+        return (child) -> add!(box, child)
+    end
+end
+
+aside(::Val{:h}, args...) = aside(Val(:horizontal), args...)
+
+function aside(size::Int = 130, type::Symbol = :vertical, reverse::Bool = false)
+    aside(Val(type), size, reverse)
+end
+
+const CURRENT_LAYOUT = Function[nolayout]
+
+function uselayout!(layout::Function)
+    CURRENT_LAYOUT[1] = layout
+end
+
+function create(w::T) where T <: GtkWidget
+    app = get_current_app()
+    app.add(w)
+    return w
+end
+
+macro create(ex)
+    :( create($(esc(ex))) )
+end
+
+macro layout(layout)
+    :( uselayout!($(esc(layout))) )
 end
